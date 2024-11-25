0d3e248f5c69c603da71d089ac8bca4da1842e63
Vladyslav Tokarev <tokarevvladyslav1@gmail.com>
2024-11-25 19:55:16 +0200

    11

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Wordldk!')
+  res.send('Hello Word!')
 })
 
 app.listen(port, () => {
